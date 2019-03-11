8fed4eb318c7c2c1c6794923c9a8d44fc942af43
gp7 <gp7@sanger.ac.uk>
2019-03-11 00:50:51 +0000

    fix

--- src/Get_fasta_from_bed12.py
+++ src/Get_fasta_from_bed12.py
@@ -22,18 +22,16 @@ def main(bed12):
     transcripts_seq = defaultdict(str)
 
     for row in csv.reader(open(bed12), delimiter = '\t'):
+        start = int(row[1])
+        end = int(row[2])
+        strand = row[5]
+        bn = int(row[9])
+        chrom = row[0]
+        transcript = row[3]
+        blocksizes = map(int, row[10].strip(",").split(","))
+        qstarts = map (int, row[11].strip(",").split(","))
 
-		start = int(row[1])
-		end = int(row[2])
-		strand = row[5]
-		bn = int(row[9])
-		chrom = row[0]
-		transcript = row[3]
-
-		blocksizes = map(int, row[10].strip(",").split(","))
-		qstarts = map (int, row[11].strip(",").split(","))
-
-		for q, b in zip(qstarts, blocksizes):
+        for q, b in zip(qstarts, blocksizes):
 
 
 			estart = start + q
