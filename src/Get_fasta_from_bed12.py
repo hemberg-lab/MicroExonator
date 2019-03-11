7800fe9b31ca1f9ba54c117e36559d70bf03bfb4
gp7 <gp7@sanger.ac.uk>
2019-03-11 00:36:49 +0000

    get fasta form bed12

---
+++ src/Get_fasta_from_bed12.py
@@ -0,0 +1,65 @@
+import sys
+import csv
+from collections import defaultdict
+
+
+def Genomictabulator(fasta):
+
+	print >> sys.stderr, "Cargando genoma en la memoria RAM ...",
+
+	f = open(fasta)
+
+	for chrfa in SeqIO.parse(f, "fasta"):
+		Genome[chrfa.id] = chrfa.seq
+
+	print >> sys.stderr, "OK"
+
+	f.close()
+
+
+def main(bed12):
+
+    transcripts_seq = defaultdict(str)
+
+    for row in csv.reader(open(bed12), delimiter = '\t'):
+
+		start = int(row[1])
+		end = int(row[2])
+		strand = row[5]
+		bn = int(row[9])
+		chrom = row[0]
+		transcript = row[3]
+
+		blocksizes = map(int, row[10].strip(",").split(","))
+		qstarts = map (int, row[11].strip(",").split(","))
+
+		for q, b in zip(qstarts, blocksizes):
+
+
+			estart = start + q
+			eend = start + q + b
+			elength = eend - estart
+
+			exon_seq = Genome[chrom][estart:eend]
+
+
+			exon_seq = str(dn).upper()
+
+            transcripts_seq[(transcript, strand)] += exon_seq
+
+
+    for key_value in transcripts_seq.items():
+
+        transcript_strand, seq = key_value
+        transcript, strand = transcript_strand
+
+		if strand=="-":
+			seq = seq.reverse_complement()
+
+        print(">" + transcript)
+        print(seq)
+
+
+if __name__ == '__main__':
+    Genomictabulator(sys.argv[1])
+    main(sys.argv[2])
