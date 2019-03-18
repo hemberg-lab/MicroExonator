aefd4cb05ec314ba9552d48ec3c1ff800f483983
Ilias Georgakopoulos-Soares <igs@sanger.ac.uk>
2019-03-18 21:22:45 +0000

    finishing stats

--- src/stats/discovery_stats.py
+++ src/stats/discovery_stats.py
@@ -1,88 +1,65 @@
-
-import sys
-import csv
-
+import sys,csv,pdb
 
 ##### Use as #####
-
     #python bed12 MicroExonator_high_qual ME_len
-
     #Compatible with python2 and python3
-
 ######
 
+#def main(bed12, ME_high_cual , ME_len):
 
-def main(bed12, ME_high_cual , ME_len):
+def bed12_microexon_extract(bed12_path,ME_len):
+	ME_annotated = set()
+	for row in csv.reader(open(bed12_path), delimiter = '\t'):
+		blocksizes = list(map(int, row[10].strip(",").split(",")))
+		qstarts = list(map(int, row[11].strip(",").split(",")))
 
-    ME_annotated = set([])
-    ME_MicroExonator = set([])
+		start = int(row[1])
+		end = int(row[2])
+		strand = row[5]
+		bn = int(row[9])
+		chrom = row[0]
 
-    with csv.reader(open(bed12) as  db:
+		for q1, q2, q3, b1, b2, b3 in zip(qstarts, qstarts[1:] , qstarts[2:], blocksizes, blocksizes[1:], blocksizes[2:]):
 
-        reader = csv.reader( high_qual_filters , delimiter = '\t') )
+			estart = start + q2
+			eend = start + q2 + b2
+			elength = eend - estart
+			exon = (chrom, strand, str(estart), str(eend))
+ 	                transcript = row[3]
+			SJ_start = start + q1 + b1
+			SJ_end = start + q3
+			ME_intron = " ".join([chrom, str(SJ_start), str(SJ_end), "SJ", "0", strand])
 
-		for row in csv.reader(open(bed12), delimiter = '\t'):
+			if elength <= ME_len:
+               			ME_annotated.add(exon)
 
+	return ME_annotated
 
-			blocksizes = list(map(int, row[10].strip(",").split(",")))
-			qstarts = list(map(int, row[11].strip(",").split(",")))
-
-
-			start = int(row[1])
-			end = int(row[2])
-			strand = row[5]
-			bn = int(row[9])
-			chrom = row[0]
-
-
-			for q1, q2, q3, b1, b2, b3 in zip(qstarts, qstarts[1:] , qstarts[2:], blocksizes, blocksizes[1:], blocksizes[2:]):
-
-
-				estart = start + q2
-				eend = start + q2 + b2
-				elength = eend - estart
-				exon = (chrom, strand, str(estart), str(eend))
-                transcript = row[3]
-				SJ_start = start + q1 + b1
-				SJ_end = start + q3
-				ME_intron = " ".join([chrom, str(SJ_start), str(SJ_end), "SJ", "0", strand])
-
-				if elength <= ME_len:
-
-                    ME_annotated.add(exon)
-
-
-    with csv.reader(open(ME_high_cual) as  high_qual_filters:
-
-        reader = csv.DictReader( high_qual_filters , delimiter = '\t') )
-
+def microexonator_output_reader(high_qual_filters):
+	ME_MicroExonator = set()
+        reader = csv.reader(open(high_qual_filters), delimiter = '\t')
+	next(reader)
         for row in reader:
+		ME = row[0]
+            	chrom = "_".join(ME.split("_")[:-3])  # To avoid errors with chrom_
+            	strand, start, end = ME.split("_")[-3:]
+            	ME = (chrom, strand, start, end)
+            	ME_MicroExonator.add(ME)
+	return ME_MicroExonator
 
-            # Hearder is "ME", "transcript", "sum_total_coverage", "total_SJs", "total_coverages", "len_micro_exon_seq_found", "micro_exon_seq_found", "total_number_of_micro_exons_matches", "U2_scores",  "mean_conservations_vertebrates", "P_MEs", "total_ME",   "ME_P_value", "ME_type"
+def compare(set_Microexonator,set_consensus):
+	set_novel = set_Microexonator-set_consensus
+	datafile=open("Microexons.not_consensus","w")
+	for line in set_novel:
+		datafile.write('\t'.join([str(x) for x in line])+'\n')
+	datafile.close()
+	return
 
-            ME = reader["ME"]
-
-            chrom = "_".join(ME.split("_")[:-3])  # To avoid errors with chrom_
-            starnd, start, end = ME.split("_")[-3:]
-
-            ME = (chrom, strand, start, end)
-
-            ME_MicroExonator.add(ME)
-
-
-    ######## TO DO #######
-
-    # Operate sets of microexos found at annotation (bed12) -> stored as a set in ME_annotated
-    # and ME found by MicroExonator -> stored as a set in ME_MicroExonator
-
-    ## Hint lock for set operation in python
-
-    ### https://docs.python.org/2/library/sets.html
-
-
-    ### generate percent of overlap, and do plots (maybe ven diagram)
-
-    ######################
 
 if __name__ == '__main__':
-	main (sys.argv[1], sys.argv[2], int(sys.argv[3]) )
+	consensus_file = sys.argv[1]
+	MicroExonator_file = sys.argv[2]
+	length_limit =int(sys.argv[3]) 
+	Consensus_microexons = bed12_microexon_extract(consensus_file,length_limit)
+	MicroExonator_microexons=microexonator_output_reader(MicroExonator_file)
+	compare(MicroExonator_microexons,Consensus_microexons)
