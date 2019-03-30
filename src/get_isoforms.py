94a1bedd9bd9aa3ac8084e18626febf24497296e
geparada <geparada88@gmail.com>
2019-03-30 13:07:59 +0000

    Update get_isoforms.py

--- src/get_isoforms.py
+++ src/get_isoforms.py
@@ -82,8 +82,7 @@ def main(annotation_bed12, annotation_gtf, out_filtered_ME):
             tag_dict = dict()
 
             for t in tags:
-		print(t)
-			
+                print(t)	
                 field, value = t.split()
                 value = value.strip('"')
                 tag_dict[field] = value
