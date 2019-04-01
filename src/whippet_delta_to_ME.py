87367c917e3c75cad328dd1bd4b61531b3491225
geparada <geparada88@gmail.com>
2019-04-01 15:39:12 +0100

    Update whippet_delta_to_ME.py

--- src/whippet_delta_to_ME.py
+++ src/whippet_delta_to_ME.py
@@ -98,9 +98,9 @@ def main(jls_exons_tab, delta, high_qual_ME ):
 
 
                 #if exon_ID in MEs:
-                if !exon_ID="":
+                if exon_ID!="":
           
-                    "\t".join([row[x] for x in header] + [exon_ID]) 
+                    print("\t".join([row[x] for x in header] + [exon_ID]))
           
 
 if __name__ == '__main__':
