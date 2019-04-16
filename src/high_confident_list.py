7002d2510fc2e95318740fae97333f2bce99c656
geparada <geparada88@gmail.com>
2019-04-16 16:52:01 +0100

    Update high_confident_list.py

--- src/high_confident_list.py
+++ src/high_confident_list.py
@@ -219,7 +219,7 @@ def main(gene_model_bed12, out_filtered_ME_cov, out_filtered_ME, out_low_scored_
                     #ME_black_list.write(row["ME"]+"\n")
                     pass
 
-                else row["micro_exon_seq_found"] in SJ_end_seqs::
+                else row["micro_exon_seq_found"] in SJ_end_seqs:
                     print(row["ME"], row["transcript"], row["sum_total_coverage"], row["total_SJs"], row["total_coverages"], row["len_micro_exon_seq_found"], row["micro_exon_seq_found"], row["total_number_of_micro_exons_matches"], row["U2_scores"], row["mean_conservations_vertebrates"], row["P_MEs"], row["total_ME"], row["ME_P_value"], row["ME_type"], sep="\t")
 
 
