ebd24c2e834abc2b99ea7afcbf64fd8c97d1f52c
geparada <geparada88@gmail.com>
2019-05-09 18:53:02 +0100

    Update get_isoforms.py

--- src/get_isoforms.py
+++ src/get_isoforms.py
@@ -59,7 +59,7 @@ def main(annotation_bed12, annotation_gtf, out_filtered_ME, chrM):
         for row in reader:
 
             chrom = "_".join(row["ME"].split("_")[:-3])
-            starnd, start, end = row["ME"].split("_")[-3:]
+            strand, start, end = row["ME"].split("_")[-3:]
 
             ME_start = [chrom, start, strand]
             ME_end = [chrom, end, strand]
