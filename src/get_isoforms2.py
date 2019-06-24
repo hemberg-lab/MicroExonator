82ab88791e4ede6bdf9743b08f737b073c2e17f5
geparada <geparada88@gmail.com>
2019-06-24 17:08:09 +0100

    Update get_isoforms2.py

--- src/get_isoforms2.py
+++ src/get_isoforms2.py
@@ -471,7 +471,7 @@ def main(annotation_bed12, annotation_gtf, out_filtered_ME, chrM):
                 
                 for sec_ME in list(secondary_ME["_".join([e_chrom, e_strand, str(e_start-1), str(e_end)])]):
 				
-					transcript_secondary_exons_pairs.add((sec_ME, e, transcript_id))
+                    transcript_secondary_exons_pairs.add((sec_ME, e, transcript_id))
 				
 ####
 								
