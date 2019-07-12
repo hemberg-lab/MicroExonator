10685d23dd9de35df5d87bc091c8cc69b93c8c7e
geparada <geparada88@gmail.com>
2019-07-12 21:17:02 +0100

    Update Snakepool.py

--- rules/Snakepool.py
+++ rules/Snakepool.py
@@ -115,7 +115,7 @@ for compare_name in cluster_compare.keys():  #Getting the target files - key = c
 rule snakepool:   # This rule execute all the nesesary rules to produce the target files
    input:
     #target_pool_delta ,#target files
-    expand("Whippet/Delta/Single_Cell/Unpooled/{compare_name}.diff.gz", compare_name=compare_names)
+    expand("Whippet/Delta/Single_Cell/Unpooled/{compare_name}.diff.microexons", compare_name=compare_names)
 
 
 ##### Single cell ####
