ecf8082481e883bd89073bb8cb311f8a00eb62a3
geparada <geparada88@gmail.com>
2020-05-26 23:52:19 +0100

    Update Snakepool.py

--- rules/Snakepool.py
+++ rules/Snakepool.py
@@ -376,11 +376,13 @@ if str2bool(config.get("cluster_sashimi", False)):
     
     
     rule get_sig_nodes:
-        input:
-            'Whippet/Delta/Single_Cell/Sig_nodes/{compare_name}.txt'
+        #input:
+        #    expand('Whippet/Delta/Single_Cell/Sig_nodes/{compare_name}.txt')
         output:
-            #expand("{sashimi}.txt", sashimi=sashimis)
-            temp(lambda w: expand("Whippet/ggsashimi/{compare_name}/{gene_node_strand}.txt",  gene_node_strand=compare_sig_nodes[w.compare_name]))
+            expand("{sashimi}.txt", sashimi=sashimis)
+            #lambda w, input:  input.split("/")[-1].split[
+            #"Whippet/ggsashimi/{compare_name}"
+            #temp(lambda w: expand("Whippet/ggsashimi/{compare_name}/{gene_node_strand}.txt",  gene_node_strand=compare_sig_nodes[w.compare_name]))
         shell:
             "python src/write_sig_node_files.py {input}"
     
