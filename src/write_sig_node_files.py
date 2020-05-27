b14c374cc2f2ff9dba0f1a7ed813eb227237d875
geparada <geparada88@gmail.com>
2020-05-27 01:21:23 +0100

    Update write_sig_node_files.py

--- src/write_sig_node_files.py
+++ src/write_sig_node_files.py
@@ -1,1 +1,21 @@
+from snakemake.utils import min_version
+import glob, os, csv
 
+
+for path in glob.glob(snakemake.params[0]):
+  
+  compare_name = path.split("/")[-1].split(".")[0]
+  
+  with open(path) as file  :
+    
+    reader = csv.DictReader(file, delimiter="\t")
+    
+    for row in reader:
+      
+      with open("Whippet/ggsashimi/" + compare_name + "/" + "_".join([row["Gene"], row["Node"], row["Strand"], ".txt"] ), "w") as output:
+        
+        fwrite(row)
+      
+        
+  
+  
