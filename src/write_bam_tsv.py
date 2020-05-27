812ddc472934ccfeb92f89b16a44187ca049d2db
geparada <geparada88@gmail.com>
2020-05-27 01:37:32 +0100

    Update write_bam_tsv.py

--- src/write_bam_tsv.py
+++ src/write_bam_tsv.py
@@ -2,13 +2,21 @@ from snakemake.utils import min_version
 import csv
 
 
-with snakemake.input[0] as f:
-  
-  reader = csv.DictReader(f, delimiter="\t")
-  
-  for row in reader:
-  
+with snakemake.input[0] as run:   #Populating the dictionaries
 
-  
-  
-  
+    run_metadata = csv.DictReader(run, delimiter="\t")
+
+    for row in run_metadata:
+        
+        with open("Whippet/ggsashimi/" + row["Compare_ID"] + "/" + row["Compare_ID"] + ".tvs", "w") as out:
+
+            A_cluster_names = []
+            B_cluster_names = []
+            
+            for c in row["A.cluster_names"].split(","):
+                
+                out.write(c + "\t" + "Whippet/BAM/Merge/" + c + "sort.bam" + "\n")
+
+            for c in row["B.cluster_names"].split(","):
+
+                out.write(c + "\t" + "Whippet/BAM/Merge/" + c + "sort.bam" + "\n")
