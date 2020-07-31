776212523dbdd453c5d7acd5ec4c674ae97d3345
geparada <geparada88@gmail.com>
2020-07-31 05:31:51 +0100

    Update Snakepool_BetaDist.R

--- src/Snakepool_BetaDist.R
+++ src/Snakepool_BetaDist.R
@@ -104,8 +104,7 @@ for (i in 1:nrow(run_metadata)) {
   
   fwrite(out, file= paste0(out_dir, run_metadata[i, Compare_ID], ".txt") , append = FALSE, quote = "auto", sep = "\t",  row.names = FALSE, col.names = TRUE)
 
-
-
+}
 
 snakepool_BetaDist(cdf_t, min.p.mean, min_rep,  
                    path_run_metatda,
