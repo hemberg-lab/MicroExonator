f090b58572dc6b4ac7e4479b811d116581cd4f17
geparada <geparada88@gmail.com>
2020-10-26 14:43:11 +0000

    Update coverage_sample_filter.py

--- src/coverage_sample_filter.py
+++ src/coverage_sample_filter.py
@@ -22,8 +22,7 @@ with open("test.txt", "w") as out:
            
 
             for row in reader:
-                
-                print(row)
+
 
                 FILE_NAME, ME, total_SJs, ME_SJ_coverages, sum_ME_coverage, sum_ME_SJ_coverage_up_down_uniq, sum_ME_SJ_coverage_up, sum_ME_SJ_coverage_down, SJ_coverages, sum_SJ_coverage, is_alternative_5, is_alternative_3, alternatives_5, cov_alternatives_5, total_cov_alternatives_5, alternatives_3, cov_alternatives_3, total_cov_alternatives_3 = row
 
