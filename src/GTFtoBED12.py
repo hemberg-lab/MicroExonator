670b36e7309f8055bd05bd4f0673dce34f317673
geparada <geparada88@gmail.com>
2020-11-12 01:47:57 +0000

    Update GTFtoBED12.py

--- src/GTFtoBED12.py
+++ src/GTFtoBED12.py
@@ -2,112 +2,60 @@ import sys
 import csv
 from collections import defaultdict
 
+def main(gtf_file):
+	
+	with open(gtf_file) as gtf:
 
+        transcript_blocksizes = defaultdict(list)
+        transcript_qstarts = defaultdict(list)
+        transcript_coords = dict()
 
-keys = set([])
-exon_start = []
-exon_size = []
-transcript_start_end = {}
-transcript_id_info = {}
+        reader = csv.reader(gtf, delimiter="\t")
 
-def converter (GTF):
-	#reader = csv.reader(open(GTF), delimiter = '\t')
+        for row in reader:
 
-	f = open(GTF)
-	for line in f:
+            if row[0][0]!="#":
 
-	    if line[0]!="#":
-                    
-                row = line.replace(";","\t").replace(" ","\t").replace("\t\t","\t").split("\t")
-                chr = row[0]
+                chrom = row[0]
                 group = row[1]
                 blocktype = row[2]
                 block_start = int(row[3]) - 1
                 block_end = int(row[4])
                 block_size = block_end - block_start
                 strand = row[6]
-                        
-                if blocktype == 'transcript':
+                
+                tags = row[8].strip(" ").split(";")
+                for t in tags:
+                    pair =  t.strip(" ").split(" ")
+                    if pair!=['']:
+                        ID_type, ID  = pair
+                        if ID_type == "transcript_id":
+                            transcript = ID.strip('"')
 
-                    #transcript_start_end[transcript_id] = (block_start, block_end)
-                    try:
-                        gene_id = row[9]
-                        transcript_id = row[11].strip('"')
-                        transcript_start_end[transcript_id] = (block_start, block_end)
-                    except IndexError:
-                        pass
-                    except ValueError:
-                        pass
+                if blocktype == 'transcript':
+                    transcript_coords[transcript] = (chrom, block_start, block_end, strand)
 
                 if blocktype == 'exon':
-                    keys.add(transcript_id)
-                    transcript_id_info[transcript_id] = (chr, group, blocktype, strand, gene_id)
-                    exon_start.append((transcript_id, block_start))
-                    exon_size.append((transcript_id, block_size))
+                    exon_size = block_end - block_start
+                    transcript_blocksizes[transcript].append(exon_size)
+                    transcript_qstarts[transcript].append(block_start)
 
 
-	#dict_id_info = dict(transcript_id_info)
-	#dict_transcript_start_end = dict(transcript_start_end)
+        for trancript in transcript_coords:
 
-	dict_exon_start = defaultdict(list)
-	for transcript_id, block_start in exon_start:
-		dict_exon_start[transcript_id].append(block_start)
+            chrom, start, end, strand = transcript_coords[trancript]
+            n_blocks = len(transcript_blocksizes[trancript])
 
-	dict_exon_size = defaultdict(list)
-	for transcript_id, block_size in exon_size:
-		dict_exon_size[transcript_id].append(block_size)
+            if strand=="+":
+                qstarts = ",".join(map(str, [x - start for x in transcript_qstarts[trancript]] ))
+                blocksizes = ",".join(map(str, transcript_blocksizes[trancript]))
+            elif strand=="-":
+                qstarts = ",".join(map(str, [x - start for x in transcript_qstarts[trancript]][::-1] ))
+                blocksizes = ",".join(map(str, transcript_blocksizes[trancript][::-1]))
 
-	#print(keys)
+            bed12 = [chrom, start, end, transcript, "0", strand, start, end, "0", n_blocks, blocksizes, qstarts]
 
-	for key in transcript_id_info:
-
-		chr = transcript_id_info[key][0]
-		name = key
-		strand = transcript_id_info[key][3]
-		blocknum = len(dict_exon_size[key])
-		blocksizes = dict_exon_size[key]
-		exon_starts = dict_exon_start[key]
-		if strand == '-':
-			blocksizes = dict_exon_size[key][::-1]
-			exon_starts = dict_exon_start[key][::-1]
-		start = transcript_start_end[key][0]
-		end = transcript_start_end[key][1]
-		tstarts = [ x-start for x in exon_starts]
-
-		group = transcript_id_info[key][1]
-		blocktype = transcript_id_info[key][2]
-		gene_id = transcript_id_info[key][4]
-#               gene_type = transcript_id_info[key][5]
-#		gene_status = transcript_id_info[key][6]
-#		gene_name = transcript_id_info[key][7]
-#		transcript_type = transcript_id_info[key][8]
-#		transcript_status = transcript_id_info[key][9]
-#		transcript_name = transcript_id_info[key][10]
-#		level = transcript_id_info[key][11]
-
-
-		#BED12 = [chr, str(start), str(end), name.strip('"'), gene_name.strip('"'), strand, transcript_name.strip('"'), transcript_type.strip('"'), str(level), str(blocknum), ",".join(map(str, blocksizes)) + "," , ",".join(map(str, tstarts)) + ",", group, gene_id.strip('"'), gene_status.strip('"'), transcript_status.strip('"'), gene_type.strip('"') ]
-
-		BED12 = [chr, str(start), str(end), name.strip('"'), "0", strand, str(start), str(end), "0", str(blocknum), ",".join(map(str, blocksizes)), ",".join(map(str, tstarts)) ]
-
-		print "\t".join(BED12)
-
-		#chr, start, end, name, gene_name, strand, transcript_name, transcript_type, level, blocknum, blocksizes, tstarts, group, gene_id, gene_status, transcript_status, gene_type
-
-
-
-
-#		print key, dict_exon_size[key], dict_exon_start[key], dict_id_info[key]
-
-
-	f.close()
-
-
-
-
-
-
-
+            print( "\t".join(map(str, bed12)))
 
 if __name__ == '__main__':
-	converter (sys.argv[1])
+	main(sys.argv[1])
