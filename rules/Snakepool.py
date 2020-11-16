90a2c7ff8b7aa3dfc3e48feb6d9f710fd82b461c
geparada <gp7@sanger.ac.uk>
2020-11-16 02:10:27 +0000

    identation fix

--- rules/Snakepool.py
+++ rules/Snakepool.py
@@ -602,35 +602,37 @@ def get_sasshimi_window(ME, w):
 	return(chrom + ":" + start + "-" + end)
 
 
-if str2bool(config.get("sashimi_tsv", False)):
-	
-	target_ME = set([])
-	
-	with open("Report/novel_highly_included.tsv") as tsv:
-					 
-		reader = csv.DictReader()
-		for row in reader:
-			if float(row["mean_PSI"])>0.9:
-				target_ME.add(row["ME"])
-			
-	rule ggsashmi_bulk_scripts:
-		input:
-			node = "ggsashimi/{ME}.txt",
-			gtf = config["Gene_anontation_GTF"]
-			#bams = lambda w: expand("Whippet/BAM/Merge/{cluster}.sort.bam",  cluster=compare_all_clusters[w.compare_name])
-        params:
-			tsv = config["sashimi_tsv"]
-			region = lambda w: get_sasshimi_window(w.ME, 1000),
-			out = "ggsashimi/{ME}"
-		output:
-			#"ggsashimi/{gene}_{node}_{strand}.sh"
-			"ggsashimi/{ME}.pdf"
-		shell:
-			"python src/sashimi-plot.py -b {params.tsv} -c {params.region} -g {input.gtf} -o {params.out} > {output} "
+if "sashimi_tsv" in config:
+#if True in config:
 
-	rule get_sashimis_bulk:
-		input:
-			expand("ggsashimi/{ME}.pdf", ME=target_ME)
+    target_ME = set([])
+	
+    with open("Report/novel_highly_included.tsv") as tsv:
+					 
+        reader = csv.DictReader()
+        for row in reader:
+            if float(row["mean_PSI"])>0.9:
+                target_ME.add(row["ME"])
+
+        rule ggsashmi_bulk_scripts:
+	    #input:
+		#node = "ggsashimi/{ME}.txt",
+		#gtf = config["Gene_anontation_GTF"],
+                #tsv = config["sashimi_tsv"]
+		#bams = lambda w: expand("Whippet/BAM/Merge/{cluster}.sort.bam",  cluster=compare_all_clusters[w.compare_name])
+            params:
+                gtf = config["Gene_anontation_GTF"],
+                tsv = config["sashimi_tsv"],
+                region = lambda w: get_sasshimi_window(w.ME, 1000),
+                out = "ggsashimi/{ME}"
+            output:
+                "ggsashimi/{ME}.pdf"
+            shell:
+                "python src/sashimi-plot.py -b {params.tsv} -c {params.region} -g {params.gtf} -o {params.out}"
+
+        rule get_sashimis_bulk:
+            input:
+                expand("ggsashimi/{ME}.pdf", ME=target_ME)
 
 
 ######
