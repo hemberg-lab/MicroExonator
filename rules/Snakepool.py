30fda3a067728cdf11e2b3cd86854d4b74470557
geparada <geparada88@gmail.com>
2020-11-16 00:27:07 +0000

    sashimi cell lines

--- rules/Snakepool.py
+++ rules/Snakepool.py
@@ -589,7 +589,50 @@ if str2bool(config.get("cluster_sashimi", False)):
             expand("{sashimi}.pdf", sashimi=sashimis)
             
 #chr:start-end
-    
+
+#####
+
+def get_sasshimi_window(ME, w):
+
+	ME_strand, ME_start, ME_end = ME.split("_")[-3:]
+	ME_chrom =  "_".join(ME.split("_")[:-3])
+					
+	start = str(int(ME_start)-w)
+	end = str(int(ME_start)+w)
+	return(chrom + ":" + start + "-" + end)
+
+
+if str2bool(config.get("sashimi_tsv", False)):
+	
+	with open("Report/novel_highly_included.tsv") as tsv:
+					 
+		reader = csv.DictReader()
+		for row in reader:
+			
+			ME= row["ME"]
+			ME_strand, ME_start, ME_end = ME.split("_")[-3:]
+			ME_chrom =  "_".join(ME.split("_")[:-3])
+			
+	rule ggsashmi_bulk_scripts:
+		input:
+			node = "Whippet/ggsashimi/{compare_name}/{ME}.txt",
+			tsv = "Whippet/ggsashimi/{compare_name}/{compare_name}.tvs",
+			gtf = config["Gene_anontation_GTF"],
+			bams = lambda w: expand("Whippet/BAM/Merge/{cluster}.sort.bam",  cluster=compare_all_clusters[w.compare_name])
+        params:
+			region = lambda w: get_sasshimi_window(w.ME, 1000),
+			out = "Whippet/ggsashimi/{compare_name}/{gene}_{node}_{strand}"
+		output:
+			"ggsashimi/{gene}_{node}_{strand}.sh"
+			#"Whippet/ggsashimi/{compare_name}/{gene}_{node}_{strand}.pdf"
+		shell:
+			"python src/sashimi-plot.py -b {input.tsv} -c {params.region} -g {input.gtf} -o {params.out} > {output} "
+
+	rule get_sashimis_bulk:
+		input:
+			expand("ggsashimi/{sashimi}.pdf", sashimi=ashimis_bulk)
+
+
 
 ######
     
