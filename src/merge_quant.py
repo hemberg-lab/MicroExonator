9336994d37d798af56c201116a96d6b192d4c550
geparada <gp7@sanger.ac.uk>
2021-02-01 18:36:49 +0000

    fix gz output

--- src/merge_quant.py
+++ src/merge_quant.py
@@ -9,12 +9,12 @@ csv.field_size_limit()
 
 
 def main(mode, out_file, file_list  ):
-    with gzip.open(out_file, 'wb') as out:
+    with gzip.open(out_file, 'wt') as out:
 
         for file in file_list:
 
             with gzip.open(file, mode="rt") as f:
-                
+
                 if mode=="Isoform" or mode=="Gene":
                     header = ["Sample", mode, "TpM", "Read_Counts"]
                 elif mode=="PSI":
@@ -22,7 +22,7 @@ def main(mode, out_file, file_list  ):
 
                 writer = csv.DictWriter(out, fieldnames=header, extrasaction='ignore', delimiter="\t")
                 writer.writeheader()
-                
+
                 sample = file.strip(snakemake.params["trim"])
 
                 #sample = "".join(file.split("/")[-1].split(".")[:-2])  #files needs to finish with *.fastq.gz
@@ -33,7 +33,7 @@ def main(mode, out_file, file_list  ):
                     row["Sample"] = sample
                     writer.writerow(row)
 
-print(snakemake.input)                    
-                    
+#print(snakemake.input)
+
 if __name__ == '__main__':
     main(snakemake.params["feature"], snakemake.output["merged"],  snakemake.input["files"])
