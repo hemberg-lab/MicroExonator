e3923f074980209b2480de74e6d6853d147fa275
geparada <geparada88@gmail.com>
2021-03-21 14:33:43 +0000

    Update coverage_sample_filter2.py

--- src/coverage_sample_filter2.py
+++ src/coverage_sample_filter2.py
@@ -9,7 +9,7 @@ with open(snakemake.output[0], 'w', newline='') as csvfile:
     #writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter="\t")
     #writer.writeheader()
     
-    files = [csv.DictReader(gzip.open(i,'rb'), delimiter="\t") for i in snakemake.input["PSI_files"] ]
+    files = [csv.DictReader(gzip.open(i,'rt'), delimiter="\t") for i in snakemake.input["PSI_files"] ]
     
     for rows in zip(*files):
 
