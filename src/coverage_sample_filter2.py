18ef15d021737683771367f46a8cfa2b95151407
geparada <geparada88@gmail.com>
2021-03-21 12:52:25 +0000

    Update coverage_sample_filter2.py

--- src/coverage_sample_filter2.py
+++ src/coverage_sample_filter2.py
@@ -8,7 +8,6 @@ with open(snakemake.output[0], 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter="\t")
     writer.writeheader()
     
-    fieldnames = ["File", "Unique_ME_reads", "ME_coords", "SJ_coords", "ME_coverages", "SJ_coverages", "PSI", "CI_Lo", "CI_Hi", "Alt5" ,"Alt3", "Alt5_coverages", "Alt3_coverages"]
     files = [csv.DictReader(open(i), delimiter="\t") for i in filenames]
     
     for rows in zip(*files):
@@ -23,6 +22,5 @@ with open(snakemake.output[0], 'w', newline='') as csvfile:
             
             if int(row["Unique_ME_reads"])>=min_read_per_sample:
                 detected_files += 1
-                
-            
+     
         csvfile.write("\t".join([ME, str(detected_files)]) + "\n")
