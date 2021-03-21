6245058e349cbbb3e30281614170a8b92c95c763
geparada <geparada88@gmail.com>
2021-03-21 13:20:25 +0000

    Update high_confident_list.py

--- src/high_confident_list.py
+++ src/high_confident_list.py
@@ -3,7 +3,7 @@ import sys
 from collections import defaultdict
 from Bio import SeqIO
 from Bio.Seq import Seq
-
+from snakemake.utils import min_version
 
 Genome = {}
 
@@ -20,7 +20,7 @@ def Genomictabulator(fasta):
 
 
 
-def main(gene_model_bed12, out_filtered_ME_cov, out_filtered_ME, out_low_scored_ME):
+def main(gene_model_bed12, out_filtered_ME, out_low_scored_ME, PSI_files):
 
     estart_exons = defaultdict(set)
     eend_exons = defaultdict(set)
@@ -31,7 +31,7 @@ def main(gene_model_bed12, out_filtered_ME_cov, out_filtered_ME, out_low_scored_
     high_confident_ME = []
 
     with open(gene_model_bed12) as bedfile, \
-        open(out_filtered_ME_cov) as ME_out_cov, \
+        #open(out_filtered_ME_cov) as ME_out_cov, \
         open(out_filtered_ME) as ME_out1, \
         open(out_low_scored_ME) as ME_low1, \
         open(out_filtered_ME) as ME_out, \
@@ -97,19 +97,23 @@ def main(gene_model_bed12, out_filtered_ME_cov, out_filtered_ME, out_low_scored_
         ## Summing exon coverage
 
 
-        reader = csv.reader(ME_out_cov, delimiter="\t")
+        #reader = csv.reader(ME_out_cov, delimiter="\t")
+        
+        for file in PSI_files:
+            
+            reader = csv.DictReader(file, delimiter="\t")
 
-        for row in reader:
-            FILE_NAME, ME, total_SJs, ME_SJ_coverages, sum_ME_coverage, sum_ME_SJ_coverage_up_down_uniq, sum_ME_SJ_coverage_up, sum_ME_SJ_coverage_down, SJ_coverages, sum_SJ_coverage, is_alternative_5, is_alternative_3, alternatives_5, cov_alternatives_5, total_cov_alternatives_5, alternatives_3,  cov_alternatives_3, total_cov_alternatives_3 = row
-            chrom = "_".join(ME.split("_")[:-3]) 
-            strand, estart, eend = ME.split("_")[-3:]
-            exon = (chrom, strand, estart, eend)
+            for row in reader:
 
-            sum_ME_SJ_coverage_up = int(sum_ME_SJ_coverage_up)
-            sum_ME_SJ_coverage_down = int(sum_ME_SJ_coverage_down)
+                #FILE_NAME, ME, total_SJs, ME_SJ_coverages, sum_ME_coverage, sum_ME_SJ_coverage_up_down_uniq, sum_ME_SJ_coverage_up, sum_ME_SJ_coverage_down, SJ_coverages, sum_SJ_coverage, is_alternative_5, is_alternative_3, alternatives_5, cov_alternatives_5, total_cov_alternatives_5, alternatives_3,  cov_alternatives_3, total_cov_alternatives_3 = row
+                #chrom = "_".join(row["ME"].split("_")[:-3]) 
+                #strand, estart, eend = ME.split("_")[-3:]
+                #exon = (chrom, strand, estart, eend)
+                #sum_ME_SJ_coverage_up = int(sum_ME_SJ_coverage_up)
+                #sum_ME_SJ_coverage_down = int(sum_ME_SJ_coverage_down)
 
-            total_ME_up[ME] += sum_ME_SJ_coverage_up
-            total_ME_down[ME] += sum_ME_SJ_coverage_down
+                total_ME_up[row["ME"]] += int(row["sum_ME_SJ_coverage_up")
+                total_ME_down[row["ME"]] += int(row["sum_ME_SJ_coverage_down"])
 
         ambiguous = open("Report/out.ambiguous.txt", "w")
 	
@@ -231,7 +235,6 @@ def main(gene_model_bed12, out_filtered_ME_cov, out_filtered_ME, out_low_scored_
                     print(row["ME"], row["transcript"], row["sum_total_coverage"], row["total_SJs"], row["total_coverages"], row["len_micro_exon_seq_found"], row["micro_exon_seq_found"], row["total_number_of_micro_exons_matches"], row["U2_scores"], row["mean_conservations_vertebrates"], row["P_MEs"], row["total_ME"], row["ME_P_value"], row["ME_type"], sep="\t")
 
 
-
 if __name__ == '__main__':
-    Genomictabulator(sys.argv[1])
-    main(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+    Genomictabulator(snakemake.input["genome"])
+    main(snakemake.input["transcriptome"], snakemake.input["out_filtered"], snakemake.input["out_low_scored"], snakemake.input["PSI_files"])
