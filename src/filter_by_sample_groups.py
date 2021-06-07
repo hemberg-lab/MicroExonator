5aa43f43569d386d0f5703271025ac2cdc7d06b8
geparada <geparada88@gmail.com>
2021-06-07 20:53:27 +0100

    Update and rename get_sample_groups.py to filter_by_sample_groups.py

--- src/filter_by_sample_groups.py
+++ src/filter_by_sample_groups.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 from snakemake.utils import min_version
 
 
-#This script gets the sample groups
+#This script gets the sample groups (output 1) and compute quantitative filters to get a list of reliable detected microexons.
 
 csv.field_size_limit(100000000)
 
