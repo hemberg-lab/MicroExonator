e5f063bf20246b37edaaaf1b3aed98dc3136217d
geparada <geparada88@gmail.com>
2021-06-07 20:58:45 +0100

    Create final_filters.py

---
+++ src/final_filters.py
@@ -0,0 +1,8 @@
+
+import sys
+import csv
+import gzip
+from collections import defaultdict
+from snakemake.utils import min_version
+
+#This gets a high quality list of microexons that were reliable discovered
