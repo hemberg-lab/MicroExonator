a1720421496c501bc211eb70bb21a7e84c8f294d
geparada <geparada88@gmail.com>
2021-06-07 20:56:23 +0100

    Create get_sparse_quants.py

---
+++ src/get_sparse_quants.py
@@ -0,0 +1,10 @@
+import sys
+import csv
+import gzip
+from collections import defaultdict
+from snakemake.utils import min_version
+
+
+#This script filter the correted quant files to get sparce quant files
+
+
