736db657ea35b7ee8fd0ddc9fa92c0550229efc4
geparada <geparada88@gmail.com>
2021-06-07 22:26:02 +0100

    Update get_sample_groups.py

--- src/get_sample_groups.py
+++ src/get_sample_groups.py
@@ -10,11 +10,37 @@ from snakemake.utils import min_version
 csv.field_size_limit(100000000)
 
 
+  
+
+  
+
+primary_clusters = defaultdict(list)
+
+
+with open("/lustre/scratch117/cellgen/team218/gp7/Micro-exons/Runs/Paper/single_cell/MicroExonator/Tasic_clustering.txt") as file:
+    
+    reader = csv.DictReader(file, delimiter="\t")
+    
+    for row in reader:
+        
+        primary_clusters[row["primary_type"]].append(row["Run_s"])
+        
+        
+with open(snakemake.params["run_metadata_sc"]) as file:
+  
+    reader = csv.DictReader(file, delimiter="\t")
+    
+    for row in reader:
+        
+        primary_clusters[snakemake.params["cell_type"]].append(snakemake.params["cells"])
+
+
+
 def partition (list_in, n):  # Function to do random pooling
     random.shuffle(list_in)
     return [list_in[i::n] for i in range(n)]
-  
-
+    
+    
 sample_group = 
 
 with open(snakemake.params["run_metatda_sc"]) as file:
