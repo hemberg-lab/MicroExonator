6c516187052ad3e433c70ab993f7d0a7ab996938
geparada <geparada88@gmail.com>
2021-06-07 20:49:45 +0100

    Create get_sample_groups.py

---
+++ src/get_sample_groups.py
@@ -0,0 +1,31 @@
+import sys
+import csv
+import gzip
+from collections import defaultdict
+from snakemake.utils import min_version
+
+
+#This script gets the sample groups
+
+csv.field_size_limit(100000000)
+
+
+def partition (list_in, n):  # Function to do random pooling
+    random.shuffle(list_in)
+    return [list_in[i::n] for i in range(n)]
+  
+
+sample_group = 
+
+with open(snakemake.params["run_metatda_sc"]) as file:
+    
+    reader = csv.DicReader(file, delimiter="\t")
+    
+    for row in reader:
+        
+
+ with open(snakemake.params["run_metatda_bulk"]) as file:
+    
+    reader = csv.DicReader(file, delimiter="\t")
+    
+    for row in reader:
