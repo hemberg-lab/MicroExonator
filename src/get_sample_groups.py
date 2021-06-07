47b15d72bc182c3a1a884d7b08ecca2c02bd4843
geparada <geparada88@gmail.com>
2021-06-07 23:23:49 +0100

    Update get_sample_groups.py

--- src/get_sample_groups.py
+++ src/get_sample_groups.py
@@ -15,75 +15,7 @@ import random
 
 csv.field_size_limit(100000000)
 
-def binP(N, p, x1, x2):
-    p = float(p)
-    q = p/(1-p)
-    k = 0.0
-    v = 1.0
-    s = 0.0
-    tot = 0.0
 
-    while(k<=N):
-            tot += v
-            if(k >= x1 and k <= x2):
-                    s += v
-            if(tot > 10**30):
-                    s = s/10**30
-                    tot = tot/10**30
-                    v = v/10**30
-            k += 1
-            v = v*q*(N+1-k)/k
-    return s/tot
-
-def calcBin(vx, vN, vCL = 95):
-    '''
-    Calculate the exact confidence interval for a binomial proportion
-    Usage:
-    >>> calcBin(13,100)
-    (0.07107391357421874, 0.21204372406005856)
-    >>> calcBin(4,7)
-    (0.18405151367187494, 0.9010086059570312)
-    '''
-    vx = float(vx)
-    vN = float(vN)
-    #Set the confidence bounds
-    vTU = (100 - float(vCL))/2
-    vTL = vTU
-
-    vP = vx/vN
-    if(vx==0):
-            dl = 0.0
-    else:
-            v = vP/2
-            vsL = 0
-            vsH = vP
-            p = vTL/100
-
-            while((vsH-vsL) > 10**-5):
-                    if(binP(vN, v, vx, vN) > p):
-                            vsH = v
-                            v = (vsL+v)/2
-                    else:
-                            vsL = v
-                            v = (v+vsH)/2
-            dl = v
-
-    if(vx==vN):
-            ul = 1.0
-    else:
-            v = (1+vP)/2
-            vsL =vP
-            vsH = 1
-            p = vTU/100
-            while((vsH-vsL) > 10**-5):
-                    if(binP(vN, v, 0, vx) < p):
-                            vsH = v
-                            v = (vsL+v)/2
-                    else:
-                            vsL = v
-                            v = (v+vsH)/2
-            ul = v
-    return (dl, ul)
   
 def partition (list_in, n):  # Function to do random pooling
     random.shuffle(list_in)
@@ -111,8 +43,6 @@ def partition (list_in, n):  # Function to do random pooling
     return [list_in[i::n] for i in range(n)]
     
 
-    
-    
 sample_group = dict()
 pseudo_pool_dict =  dict()
 
